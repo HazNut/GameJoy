c956eb06072d3011c1748a973903ceda2ba25a85
= <=>
2020-08-07 10:54:10 +0100

    Added more instructions and improved debugging output.

--- gb/gb.cpp
+++ gb/gb.cpp
@@ -85,7 +85,7 @@ void gb::emulateCycle()
 	opcode = memory[PC];
 	char opcodeStr[3];
 	_itoa_s(opcode, opcodeStr, 16);
-	printf("Opcode is %s, PC is %i.\n", opcodeStr, PC);
+	printf("\nOpcode is %s, PC is %i. ZNHC = %i%i%i%i\n", opcodeStr, PC, Zb, Nb, Hb, Cb);
 
 	switch (opcode & 0xF0) // 8 bit instructions
 	{
@@ -383,10 +383,457 @@ void gb::emulateCycle()
 			Nb = 1;
 			Hb = 1;
 		}
+		break;
 
+	case 0x30:
+		switch (opcode & 0x0F)
+		{
+		case 0x00: // JR NC, r8
+			if (Cb == 0)
+				PC += memory[PC + 1];
+			break;
+
+		case 0x01: // LD SP, d16
+			SP = (memory[PC + 1] << 8) | memory[PC + 2];
+			PC += 3;
+			break;
+
+		case 0x02: // LD (HL-), A
+			memory[(H << 8) | L] = memory[A];
+			dec(H, L);
+			PC += 1;
+			break;
+
+		case 0x03: // INC SP
+			SP += 1;
+			PC += 1;
+			break;
+
+		case 0x04: // INC (HL)
+			inc(memory[(H << 8) | L]);
+			PC += 1;
+			break;
+
+		case 0x05: // DEC (HL)
+			dec(memory[(H << 8) | L]);
+			PC += 1;
+			break;
+
+		case 0x06: // LD (HL), d8
+			memory[(H << 8) | L] = memory[PC + 1];
+			PC += 2;
+			break;
+
+		case 0x07: // SCF
+			Cb = 1;
+			Nb = 0;
+			Hb = 0;
+			PC += 1;
+			break;
+
+		case 0x08: // JR C, r8
+			if (Cb == 1)
+				PC += memory[PC + 1];
+			break;
+
+		case 0x09: // ADD HL, SP
+			HL = (H << 8) | L;
+
+			if (HL > (0xFFFF - SP))
+				Cb = 1;
+			else
+				Cb = 0;
+			Hb = checkHalfCarry(HL, SP, 1);
+			HL += SP;
+			H = HL >> 8 & 0xFF;
+			L = HL & 0xFF;
+			PC += 1;
+			Nb = 0;
+			break;
+
+		case 0x0A: // LD A, (HL-)
+			A = memory[(H << 8) | L];
+			dec(H, L);
+			PC += 1;
+			break;
+
+		case 0x0B: // DEC SP
+			SP -= 1;
+			PC += 1;
+			break;
+
+		case 0x0C: // INC A
+			inc(A);
+			PC += 1;
+			break;
+
+		case 0x0D: // DEC A
+			dec(A);
+			PC += 1;
+			break;
+
+		case 0x0E: // LD A, d8
+			A = memory[PC + 1];
+			PC += 2;
+			break;
+
+		case 0x0F: // CCF
+			Cb = ~Cb;
+			Nb = 0;
+			Hb = 0;
+			PC += 1;
+			break;
+		}
+		break;
+
+	case 0x40:
+		switch (opcode & 0x0F)
+		{
+		case 0x00: // LD B, B
+			B = B;
+			PC += 1;
+			break;
+
+		case 0x01: // LD B, C
+			B = C;
+			PC += 1;
+			break;
+
+		case 0x02: // LD B, D
+			B = D;
+			PC += 1;
+			break;
+
+		case 0x03: // LD B, E
+			B = E;
+			PC += 1;
+			break;
+
+		case 0x04: // LD B, H
+			B = H;
+			PC += 1;
+			break;
+
+		case 0x05: // LD B, L
+			B = L;
+			PC += 1;
+			break;
+
+		case 0x06: // LD B, (HL)
+			B = memory[(H << 8) | L];
+			PC += 1;
+			break;
+
+		case 0x07: // LD B, A
+			B = A;
+			PC += 1;
+			break;
+
+		case 0x08: // LD C, B
+			C = B;
+			PC += 1;
+			break;
+
+		case 0x09: // LD C, C
+			C = C;
+			PC += 1;
+			break;
+
+		case 0x0A: // LD C, D
+			C = D;
+			PC += 1;
+			break;
+
+		case 0x0B: // LD C, E
+			C = E;
+			PC += 1;
+			break;
+
+		case 0x0C: // LD C, H
+			C = H;
+			PC += 1;
+			break;
+
+		case 0x0D: // LD C, L
+			C = L;
+			PC += 1;
+			break;
+
+		case 0x0E: // LD C, (HL)
+			C = memory[(H << 8) | L];
+			PC += 1;
+			break;
+
+		case 0x0F: // LD C, A
+			C = A;
+			PC += 1;
+			break;
+		}
+		break;
+
+	case 0x50:
+		switch (opcode & 0x0F)
+		{
+		case 0x00: // LD D, B
+			D = B;
+			PC += 1;
+			break;
+
+		case 0x01: // LD D, C
+			D = C;
+			PC += 1;
+			break;
+
+		case 0x02: // LD D, D
+			D = D;
+			PC += 1;
+			break;
+
+		case 0x03: // LD D, E
+			D = E;
+			PC += 1;
+			break;
+
+		case 0x04: // LD D, H
+			D = H;
+			PC += 1;
+			break;
+
+		case 0x05: // LD D, L
+			D = L;
+			PC += 1;
+			break;
+
+		case 0x06: // LD D, (HL)
+			D = memory[(H << 8) | L];
+			PC += 1;
+			break;
+
+		case 0x07: // LD D, A
+			D = A;
+			PC += 1;
+			break;
+
+		case 0x08: // LD D, B
+			D = B;
+			PC += 1;
+			break;
+
+		case 0x09: // LD D, C
+			D = C;
+			PC += 1;
+			break;
+
+		case 0x0A: // LD D, D
+			D = D;
+			PC += 1;
+			break;
+
+		case 0x0B: // LD D, E
+			D = E;
+			PC += 1;
+			break;
+
+		case 0x0C: // LD D, H
+			D = H;
+			PC += 1;
+			break;
+
+		case 0x0D: // LD D, L
+			D = L;
+			PC += 1;
+			break;
+
+		case 0x0E: // LD D, (HL)
+			D = memory[(H << 8) | L];
+			PC += 1;
+			break;
+
+		case 0x0F: // LD D, A
+			D = A;
+			PC += 1;
+			break;
+		}
+		break;
+
+	case 0x60:
+		switch (opcode & 0x0F)
+		{
+		case 0x00: // LD H, B
+			H = B;
+			PC += 1;
+			break;
+
+		case 0x01: // LD H, C
+			H = C;
+			PC += 1;
+			break;
+
+		case 0x02: // LD H, D
+			H = D;
+			PC += 1;
+			break;
+
+		case 0x03: // LD H, E
+			H = E;
+			PC += 1;
+			break;
+
+		case 0x04: // LD H, H
+			H = H;
+			PC += 1;
+			break;
+
+		case 0x05: // LD H, L
+			H = L;
+			PC += 1;
+			break;
+
+		case 0x06: // LD H, (HL)
+			H = memory[(H << 8) | L];
+			PC += 1;
+			break;
+
+		case 0x07: // LD H, A
+			H = A;
+			PC += 1;
+			break;
+
+		case 0x08: // LD H, B
+			H = B;
+			PC += 1;
+			break;
+
+		case 0x09: // LD H, C
+			H = C;
+			PC += 1;
+			break;
+
+		case 0x0A: // LD H, D
+			H = D;
+			PC += 1;
+			break;
+
+		case 0x0B: // LD H, E
+			H = E;
+			PC += 1;
+			break;
+
+		case 0x0C: // LD H, H
+			H = H;
+			PC += 1;
+			break;
+
+		case 0x0D: // LD H, L
+			H = L;
+			PC += 1;
+			break;
+
+		case 0x0E: // LD H, (HL)
+			H = memory[(H << 8) | L];
+			PC += 1;
+			break;
+
+		case 0x0F: // LD H, A
+			H = A;
+			PC += 1;
+			break;
+		}
+		break;
+
+	case 0x70:
+		switch (opcode & 0x0F)
+		{
+		case 0x00: // LD (HL), B
+			memory[(H << 8) | L] = B;
+			PC += 1;
+			break;
+
+		case 0x01: // LD (HL), C
+			memory[(H << 8) | L] = C;
+			PC += 1;
+			break;
+
+		case 0x02: // LD (HL), D
+			memory[(H << 8) | L] = D;
+			PC += 1;
+			break;
+
+		case 0x03: // LD (HL), E
+			memory[(H << 8) | L] = E;
+			PC += 1;
+			break;
+
+		case 0x04: // LD (HL), H
+			memory[(H << 8) | L] = H;
+			PC += 1;
+			break;
+
+		case 0x05: // LD (HL), L
+			memory[(H << 8) | L] = L;
+			PC += 1;
+			break;
+
+		case 0x06: // HALT
+			IME = 0;
+			
+
+		case 0x07: // LD (HL), A
+			memory[(H << 8) | L] = A;
+			PC += 1;
+			break;
+
+		case 0x08: // LD A, B
+			A = B;
+			PC += 1;
+			break;
+
+		case 0x09: // LD A, C
+			A = C;
+			PC += 1;
+			break;
+
+		case 0x0A: // LD A, D
+			A = D;
+			PC += 1;
+			break;
+
+		case 0x0B: // LD A, E
+			A = E;
+			PC += 1;
+			break;
+
+		case 0x0C: // LD A, H
+			A = H;
+			PC += 1;
+			break;
+
+		case 0x0D: // LD A, L
+			A = L;
+			PC += 1;
+			break;
+
+		case 0x0E: // LD A, (HL)
+			A = memory[(H << 8) | L];
+			PC += 1;
+			break;
+
+		case 0x0F: // LD A, A
+			A = A;
+			PC += 1;
+			break;
+		}
+		break;
+
+	case 0x80:
+		switch (opcode & 0x0F)
+		{
+		case 0x00: // ADD A, B
+
+			break;
+		}
 	default:
-		1;
-		//printf("Unknown opcode.\n");
+		printf("Unknown opcode.\n");
 	}
 	setFlags();
 	Sleep(1000);
