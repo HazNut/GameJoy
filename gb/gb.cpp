1472ab890283965d33d4d19c79f51a49fa553b8b
= <=>
2020-08-07 01:29:06 +0100

    Added Game Boy boot sequence, more instructions, fixed case statements (didn't always break) and optimised by adding more functions for repeated code.

--- gb/gb.cpp
+++ gb/gb.cpp
@@ -3,14 +3,59 @@
 #include <fstream>
 #include <iostream>
 #include <iomanip>
+#include <Windows.h>
 
 using namespace std;
 
+// Initialize the Game Boy.
 void gb::initialize()
 {
-	PC = 0x0;
+	A = 0x01;
+	F = 0xB0;
+	B = 0x00;
+	C = 0x13;
+	D = 0x00;
+	E = 0xD8;
+	H = 0x01;
+	L = 0x4D;
+	SP = 0x014D;
+
+	memory[0xFF05] = 0x00;
+	memory[0xFF06] = 0x00;
+	memory[0xFF07] = 0x00;
+	memory[0xFF10] = 0x80;
+	memory[0xFF11] = 0xBF;
+	memory[0xFF12] = 0xF3;
+	memory[0xFF14] = 0xBF;
+	memory[0xFF16] = 0x3F;
+	memory[0xFF17] = 0x00;
+	memory[0xFF19] = 0xBF;
+	memory[0xFF1A] = 0x7F;
+	memory[0xFF1B] = 0xFF;
+	memory[0xFF1C] = 0x9F;
+	memory[0xFF1E] = 0xBF;
+	memory[0xFF20] = 0xFF;
+	memory[0xFF21] = 0x00;
+	memory[0xFF22] = 0x00;
+	memory[0xFF23] = 0xBF;
+	memory[0xFF24] = 0x77;
+	memory[0xFF25] = 0xF3;
+	memory[0xFF26] = 0xF1;
+	memory[0xFF40] = 0x91;
+	memory[0xFF42] = 0x00;
+	memory[0xFF43] = 0x00;
+	memory[0xFF45] = 0x00;
+	memory[0xFF47] = 0xFC;
+	memory[0xFF48] = 0xFF;
+	memory[0xFF49] = 0xFF;
+	memory[0xFF4A] = 0x00;
+	memory[0xFF4B] = 0x00;
+	memory[0xFFFF] = 0x00;
+
+	PC = 0x100;
 }
 
+// Load the game.
 void gb::loadGame()
 {
 	streampos size;
@@ -30,15 +75,17 @@ void gb::loadGame()
 		delete[] memblock;
 	}
 	else printf("Unable to open file\n");
+
 }
 
+// Emulate a cycle of the Game Boy.
 void gb::emulateCycle()
 {
 	// Get the opcode and execute the required instructions.
 	opcode = memory[PC];
 	char opcodeStr[3];
 	_itoa_s(opcode, opcodeStr, 16);
-	printf("Opcode is %s.\n", opcodeStr);
+	printf("Opcode is %s, PC is %i.\n", opcodeStr, PC);
 
 	switch (opcode & 0xF0) // 8 bit instructions
 	{
@@ -61,27 +108,18 @@ void gb::emulateCycle()
 			break;
 
 		case 0x03: // INC BC
-			BC = (B << 8) | C;
-			BC += 1;
-			B = BC >> 8 & 0xFF;
-			C = BC & 0xFF;
+			inc(B, C);
 			PC += 1;
 			break;
 
 		case 0x04: // INC B
-			Hb = checkHalfCarry(B, 0x1, 1);
-			B += 1;
+			inc(B);
 			PC += 1;
-			Zb = checkZero(B);
-			Nb = 0;
 			break;
 
 		case 0x05: // DEC B
-			Hb = checkHalfCarry(B, 0x1, -1);
-			B -= 1;
+			dec(B);
 			PC += 1;
-			Zb = checkZero(B);
-			Nb = 1;
 			break;
 
 		case 0x06: // LD B, d8
@@ -90,12 +128,8 @@ void gb::emulateCycle()
 			break;
 
 		case 0x07: // RLCA
-			Cb = (A >> 7) & 0b1;
-			A <<= 1;
+			rot('L', false, A);
 			PC += 1;
-			Zb = 0;
-			Nb = 0;
-			Hb = 0;
 			break;
 
 		case 0x08: // LD (a16), SP
@@ -126,27 +160,17 @@ void gb::emulateCycle()
 			break;
 
 		case 0x0B: // DEC BC
-			BC = (B << 8) | C;
-			BC -= 1;
-			B = BC >> 8 & 0xFF;
-			C = BC & 0xFF;
+			dec(B, C);
 			PC += 1;
 			break;
 
 		case 0x0C: // INC C
-			C += 1;
-			PC += 1;
-			Zb = checkZero(C);
-			Nb = 0;
-			Hb = checkHalfCarry(C, 0x1, 1);
+			inc(C);
 			break;
 
 		case 0x0D: // DEC C
-			Hb = checkHalfCarry(C, 1, -1);
-			C -= 1;
+			dec(C);
 			PC += 1;
-			Zb = 0;
-			Nb = 1;
 			break;
 
 		case 0x0E: // LD C, d8
@@ -155,14 +179,11 @@ void gb::emulateCycle()
 			break;
 
 		case 0X0F: // RRCA
-			Cb = A & 0b1;
-			A >>= 1;
+			rot('R', false, A);
 			PC += 1;
-			Zb = 0;
-			Nb = 0;
-			Hb = 0;
 			break;
 		}
+		break;
 
 	case 0x10:
 		switch (opcode & 0x0F)
@@ -184,27 +205,18 @@ void gb::emulateCycle()
 			break;
 
 		case 0x03: // INC DE
-			DE = (D << 8) | E;
-			DE += 1;
-			D = DE >> 8 & 0xFF;
-			E = DE & 0xFF;
+			inc(D, E);
 			PC += 1;
 			break;
 
 		case 0x04: // INC D
-			Hb = checkHalfCarry(D, 0x1, 1);
-			D += 1;
+			inc(D);
 			PC += 1;
-			Zb = checkZero(D);
-			Nb = 0;
 			break;
 
 		case 0x05: // DEC D
-			Hb = checkHalfCarry(D, 0x1, -1);
-			D -= 1;
+			dec(D);
 			PC += 1;
-			Zb = checkZero(D);
-			Nb = 1;
 			break;
 
 		case 0x06: // LD D, d8
@@ -213,17 +225,8 @@ void gb::emulateCycle()
 			break;
 
 		case 0x07: // RLA
-			oldCb = Cb;
-			Cb = (A >> 7) & 0b1;
-			A <<= 1;
-			if (oldCb == 1)
-				A |= 0x1;
-			else
-				A &= ~0x1;
+			rot('L', true, A);
 			PC += 1;
-			Zb = 0;
-			Nb = 0;
-			Hb = 0;
 			break;
 
 		case 0x08: // JR r8
@@ -252,27 +255,18 @@ void gb::emulateCycle()
 			break;
 
 		case 0x0B: // DEC DE
-			DE = (D << 8) | E;
-			DE -= 1;
-			D = DE >> 8 & 0xFF;
-			E = DE & 0xFF;
+			dec(D, E);
 			PC += 1;
 			break;
 
 		case 0x0C: // INC E
-			Hb = checkHalfCarry(E, 0x1, 1);
-			E += 1;
+			inc(E);
 			PC += 1;
-			Zb = checkZero(E);
-			Nb = 0;
 			break;
 
 		case 0x0D: // DEC E
-			Hb = checkHalfCarry(E, 0x1, -1);
-			E -= 1;
+			dec(E);
 			PC += 1;
-			Zb = 0;
-			Nb = 1;
 			break;
 
 		case 0x0E: // LD E, d8
@@ -281,18 +275,113 @@ void gb::emulateCycle()
 			break;
 
 		case 0x0F: // RRA
-			oldCb = Cb;
-			Cb = A & 0b1;
-			A >>= 1;
-			if (oldCb == 1)
-				A |= 0x1 << 7;
-			else
-				A &= ~(0x1 << 7);
+			rot('R', true, A);
 			PC += 1;
-			Zb = 0;
-			Nb = 0;
-			Hb = 0;
 			break;
+		}
+		break;
+
+	case 0x20:
+		switch (opcode & 0x0F)
+		{
+		case 0x00: // JR NZ, r8
+			if (Zb == 0)
+				PC += memory[PC + 1];
+			break;
+		
+		case 0x01: // LD HL, d16
+			H = memory[PC + 1];
+			L = memory[PC + 2];
+			PC += 3;
+			break;
+
+		case 0x02: // LD (HL+), A
+			memory[(H << 8) | L] = memory[A];
+			inc(H, L);
+			PC += 1;
+			break;
+
+		case 0x03: // INC HL
+			inc(H, L);
+			PC += 1;
+			break;
+
+		case 0x04: // INC H
+			inc(H);
+			PC += 1;
+			break;
+
+		case 0x05: // DEC H
+			dec(H);
+			PC += 1;
+			break;
+
+		case 0x06: // LD H, d8
+			H = memory[PC + 1];
+			PC += 2;
+			break;
+
+		case 0x07: // DAA
+		{
+			if (A > 99)
+				Cb = 1;
+			Zb = checkZero(A);
+			unsigned char d1 = A % 10;
+			A /= 10;
+			unsigned char d2 = A % 10;
+			A = (d2 << 4) | d1;
+		}
+
+		case 0x08: // JR Z, r8
+			if (Zb == 1)
+				PC += memory[PC + 1];
+			break;
+
+		case 0x09: // ADD HL, HL
+			HL = (H << 8) | L;
+
+			if (HL > (0xFFFF - HL))
+				Cb = 1;
+			else
+				Cb = 0;
+			Hb = checkHalfCarry(HL, HL, 1);
+			HL += HL;
+			H = HL >> 8 & 0xFF;
+			L = HL & 0xFF;
+			PC += 1;
+			Nb = 0;
+			break;
+
+		case 0x0A: // LD A, (HL+)
+			A = memory[(H << 8) | L];
+			inc(H, L);
+			PC += 1;
+			break;
+
+		case 0x0B: // DEC HL
+			dec(H, L);
+			PC += 1;
+			break;
+
+		case 0x0C: // INC L
+			inc(L);
+			PC += 1;
+			break;
+
+		case 0x0D: // DEC L
+			dec(L);
+			PC += 1;
+			break;
+
+		case 0x0E: // LD L, d8
+			L = memory[PC + 1];
+			PC += 2;
+			break;
+
+		case 0x0F: // CPL
+			A = ~A;
+			Nb = 1;
+			Hb = 1;
 		}
 
 	default:
@@ -300,50 +389,18 @@ void gb::emulateCycle()
 		//printf("Unknown opcode.\n");
 	}
 	setFlags();
+	Sleep(1000);
+	
 }
 
-// Set flag bits in register F.
+// Set the values of the flag bits in register F.
 void gb::setFlags()
 {
-	switch (Zb)
-	{
-	case 0:
-		F &= ~(0b1 << 7);
-		break;
-	case 1:
-		F |= 0b1 << 7;
-		break;
-	}
-
-	switch (Nb)
-	{
-	case 0:
-		F &= ~(0b1 << 6);
-		break;
-	case 1:
-		F |= 0b1 << 6;
-		break;
-	}
-
-	switch (Hb)
-	{
-	case 0:
-		F &= ~(0b1 << 5);
-		break;
-	case 1:
-		F |= 0b1 << 5;
-		break;
-	}
-
-	switch (Cb)
-	{
-	case 0:
-		F &= ~(0b1 << 4);
-		break;
-	case 1:
-		F |= 0b1 << 4;
-		break;
-	}
+	// Set each flag bit to the value it should be in the register F.
+	modifyBit(F, Zb, 7);
+	modifyBit(F, Nb, 6);
+	modifyBit(F, Hb, 5);
+	modifyBit(F, Cb, 4);
 }
 
 // Check if the half-carry bit should be set for an addition/subtraction.
@@ -359,8 +416,10 @@ int gb::checkHalfCarry(char a, char b, int mode)
 			return 1;
 		else
 			return 0;
+	return Hb;
 }
 
+// Check if a value equals 0. Used to set the zero flag bit.
 int gb::checkZero(char a)
 {
 	if (a == 0)
@@ -369,7 +428,81 @@ int gb::checkZero(char a)
 		return 0;
 }
 
-int load()
+// Increment the value stored in a register.
+void gb::inc(unsigned char &r)
 {
+	Hb = checkHalfCarry(r, 0x1, 1);
+	r += 1;
+	Zb = checkZero(r);
+	Nb = 0;
+}
 
+// Increment the value stored across two registers.
+void gb::inc(unsigned char &r1, unsigned char &r2)
+{
+	int r1r2 = (r1 << 8) | r2;
+	r1r2 += 1;
+	r1 = r1r2 >> 8 & 0xFF;
+	r2 = r1r2 & 0xFF;
+}
+
+// Decrement the value in a register.
+void gb::dec(unsigned char &r)
+{
+	Hb = checkHalfCarry(r, 0x1, -1);
+	r -= 1;
+	Zb = checkZero(r);
+	Nb = 1;
+}
+
+// Decrement the value stored across two registers.
+void gb::dec(unsigned char &r1, unsigned char &r2)
+{
+	int r1r2 = (r1 << 8) | r2;
+	r1r2 -= 1;
+	r1 = r1r2 >> 8 & 0xFF;
+	r2 = r1r2 & 0xFF;
+}
+
+// Rotate the contents of a register left or right. Can rotate normally or through carry.
+void gb::rot(char dir, bool carry, unsigned char &r)
+{
+	unsigned int oldCb = Cb;
+	if (dir == 'L')
+	{
+		Cb = (r >> 7) & 0x1;
+		r <<= 1;
+
+		if (carry)
+			if (oldCb == 1)
+				r |= 0x1;
+			else
+				r &= ~0x1;
+	}
+	else if (dir == 'R')
+	{
+		Cb = r & 0x1;
+		r >>= 1;
+		if (carry)
+			if (oldCb == 1)
+				r |= 0x1 << 7;
+			else
+				r &= ~(0x1 << 7);
+	}
+	Zb = 0;
+	Nb = 0;
+	Hb = 0;
+}
+
+// Set or reset a bit in a byte, at a given position.
+void gb::modifyBit(unsigned char &r, int val, int pos)
+{
+	if (val == 0)
+	{
+		r &= ~(0x1 << pos);
+	}
+	else if (val == 1)
+	{
+		r |= 0x1 << pos;
+	}
 }
--- gb/gb.h
+++ gb/gb.h
@@ -3,11 +3,10 @@ class gb
 {
 private:
 	unsigned char opcode;
-	unsigned char memory[65535]; // 16-bit address bus means 64kB can be addressed (??)
+	unsigned char memory[65536]; // 16-bit address bus means 64kB can be addressed (??)
 	unsigned char A, B, C, D, E, F, H, L; // Registers
 	unsigned int SP, PC; // Stack pointer and program counter
 	unsigned int HL, BC, DE;
-	unsigned int oldCb;
 	int Zb, Nb, Hb, Cb; // Flags in flag register
 	int IME;
 
@@ -18,4 +17,10 @@ public:
 	void setFlags();
 	int checkHalfCarry(char a, char b, int mode);
 	int checkZero(char a);
+	void inc(unsigned char &r);
+	void inc(unsigned char &r1, unsigned char &r2);
+	void dec(unsigned char &r);
+	void dec(unsigned char &r1, unsigned char &r2);
+	void rot(char dir, bool carry, unsigned char &r);
+	void modifyBit(unsigned char &r, int val, int pos);
 };
